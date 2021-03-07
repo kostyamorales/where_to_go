effb67c8f83c6ace11e2620a0e9c5f320752372b
Konstantin Zemlyakov <kostyamorales@gmail.com>
2021-03-07 12:59:11 +0300

    Got data for template from Place model

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,37 +1,31 @@
 from django.shortcuts import render
+from places.models import Place
 
 
 def index(request):
-    places = {
-        "type": "FeatureCollection",
-        "features": [
-            {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [37.62, 55.793676]
-                },
-                "properties": {
-                    "title": "«Легенды Москвы",
-                    "placeId": "moscow_legends",
-                    "detailsUrl": "./static/places/moscow_legends.json"
-                }
-            },
-            {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [37.64, 55.753676]
-                },
-                "properties": {
-                    "title": "Крыши24.рф",
-                    "placeId": "roofs24",
-                    "detailsUrl": "./static/places/roofs24.json"
-                }
-            }
-        ]
+    places = Place.objects.all()
+    features = {
+        'type': 'FeatureCollection',
+        'features': [],
     }
+    for place in places:
+        features['features'].append({
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [
+                    place.lon,
+                    place.lat
+                ]
+            },
+            'properties': {
+                'title': place.title,
+                'placeId': place.id,
+                'detailsUrl': './static/places/1.json'
+            }
+        })
+
     places_geojson = {
-        'places': places
+        'places': features
     }
     return render(request, 'index.html', context=places_geojson)
