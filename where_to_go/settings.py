8babab8ff1a63cfe6d86225cb43f78c247fdbd9a
Konstantin Zemlyakov <kostyamorales@gmail.com>
2021-03-10 18:48:18 +0300

    Configured STATIC_ROOT, STATICFILES_DIRS and MEDIA_ROOT

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'assets'),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
