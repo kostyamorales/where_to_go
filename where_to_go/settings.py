044331a2e8d12b833fb88e687cc7827d779ecf72
Konstantin Zemlyakov <kostyamorales@gmail.com>
2021-03-10 18:51:24 +0300

    Revert "Configured STATIC_ROOT, STATICFILES_DIRS and MEDIA_ROOT"
    This reverts commit 8babab8ff1a63cfe6d86225cb43f78c247fdbd9a.

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = ''
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'assets'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
